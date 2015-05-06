118300f6071c8ca09db609808c5aa382450be41d
CuddleMuffin007 <brennonkrogers@gmail.com>
2015-05-06 18:51:27 +0900

    edited routes and refactored anchor tags in welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-	get 'pages/welcome' => 'pages#welcome'
+	root 'pages#welcome'
+	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
