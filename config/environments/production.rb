f55ee8f17f346257c5a6c1eafa31af98686e7c70
CuddleMuffin007 <brennonkrogers@gmail.com>
2015-05-09 00:17:07 +0900

    modified production.rb to use env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
 	port: 587,
 	domain: 'gmail.com',
-	user_name: Rails.application.secrets.email,
-	password: Rails.application.secrets.password,
+	user_name: ENV['GMAIL_USERNAME'],
+	password: ENV['GMAIL_PASSWORD'],
 	authentication: 'plain',
 	enable_starttls_auto: true 
   }
