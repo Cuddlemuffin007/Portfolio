3ce006f1f2f5d3f72ece2cbc24f82221979faf47
CuddleMuffin007 <brennonkrogers@gmail.com>
2015-05-09 13:31:09 +0900

    added a little jquery magic to contact form

---
+++ app/assets/javascripts/contact_form.js
@@ -0,0 +1,17 @@
+//Place all the behaviors and hooks related to the matching controller here.
+//All this logic will automatically be available in application.js.
+//You can use CoffeeScript in this file: http://coffeescript.org/
+$( document ).ready(function() {
+	$('#contact_form_message').focus(function() {
+		if (this.value === this.defaultValue) {
+			this.value = '';
+		}
+	}).blur(function() {
+		if (this.value === '') {
+			this.value = this.defaultValue;
+		}
+	});
+	$('button').click(function () {
+		$(this).css("background-color", "#0f0");
+	});
+});
