a01a6d87441d75ac08763b57ecb78e2295ae20a3
CuddleMuffin007 <brennonkrogers@gmail.com>
2015-05-09 14:06:58 +0900

    modified contact_form.js to fix bug on heroku deployment

--- app/assets/javascripts/contact_form.js
+++ app/assets/javascripts/contact_form.js
@@ -1,14 +1,14 @@
 //Place all the behaviors and hooks related to the matching controller here.
 //All this logic will automatically be available in application.js.
 //You can use CoffeeScript in this file: http://coffeescript.org/
-$( document ).ready(function() {
+$(document).ready(function() {
 	$('#contact_form_message').focus(function() {
-		if (this.value === this.defaultValue) {
-			this.value = '';
+		if ([this.value === this.defaultValue]) {
+			[this.value = ''];
 		}
 	}).blur(function() {
-		if (this.value === '') {
-			this.value = this.defaultValue;
+		if ([this.value === '']) {
+			[this.value = this.defaultValue];
 		}
 	});
 	$('button').click(function () {
