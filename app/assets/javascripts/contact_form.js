a8f07b45a670bd57b1f0c3ca167f114a5f7e5abc
CuddleMuffin007 <brennonkrogers@gmail.com>
2015-05-10 13:14:14 +0900

    refactored jQuery code for Heroku deployment

--- app/assets/javascripts/contact_form.js
+++ app/assets/javascripts/contact_form.js
@@ -1,7 +1,7 @@
 //Place all the behaviors and hooks related to the matching controller here.
 //All this logic will automatically be available in application.js.
 //You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function() {
+var ready = function() {
 	$('#contact_form_message').focus(function() {
 		if ([this.value === this.defaultValue]) {
 			[this.value = ''];
@@ -10,4 +10,6 @@ $(document).ready(function() {
 	$('button').click(function () {
 		$(this).css("background-color", "#0f0");
 	});
-});
+}
+$(document).ready(ready);
+$(document).on('page:load', ready);
